eb039f4918bd19ae297913d06081012f3863c2b8
udit034 <udit.mehata@gmail.com>
2019-01-24 10:26:44 -0800

    Adding interactivity to the pages using jQuery

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,7 +11,28 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$("#testjs").toggleClass("active");
 	});
+
+	function projectClick(e) { 
+	    // prevent the page from reloading      
+	    e.preventDefault();
+	    // In an event handler, $(this) refers to      
+	    // the object that triggered the event      
+	    // $(this).css("background-color", "#7fff00");
+	    var containingProject = $(this).closest(".project");
+	    var description = $(containingProject).find(".project-description");
+	    if (description.length == 0) {
+	       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	    } else {
+	       description.fadeToggle("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	    }
+	};
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
